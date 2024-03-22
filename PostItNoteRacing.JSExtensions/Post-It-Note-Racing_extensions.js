4b1483e289a1c595ce40e0af6c0f04332f8f8829
Derrick Moeller <derrickmoeller@gmail.com>
2024-03-21 19:31:48 -0500

    Fixed error that occurred when arguments were null.

--- PostItNoteRacing.JSExtensions/Post-It-Note-Racing_extensions.js
+++ PostItNoteRacing.JSExtensions/Post-It-Note-Racing_extensions.js
@@ -1,9 +1,21 @@
 "use strict";
 
 function pinr_GetLeaderboardPosition(classIndex, classPosition) {
-    return $prop('PostItNoteRacing.Class_' + classIndex.toString().padStart(2, '0') + '_' + classPosition.toString().padStart(2, '0') + '_LeaderboardPosition');
+    let leaderboardPosition = $prop('PostItNoteRacing.Class_' + (classIndex ?? '00').toString().padStart(2, '0') + '_' + (classPosition ?? '00').toString().padStart(2, '0') + '_LeaderboardPosition');
+
+    if (leaderboardPosition === undefined) {
+        return null;
+    } else {
+        return leaderboardPosition;
+    }
 }
 
 function pinr_GetPropertyFromLeaderboardPosition(leaderboardPosition, propertyName) {
-    return $prop('PostItNoteRacing.Drivers_' + leaderboardPosition.toString().padStart(2, '0') + '_' + propertyName);
+    let propertyValue = $prop('PostItNoteRacing.Drivers_' + (leaderboardPosition ?? '00').toString().padStart(2, '0') + '_' + propertyName);
+
+    if (propertyValue === undefined) {
+        return null;
+    } else {
+        return propertyValue;
+    }
 }
