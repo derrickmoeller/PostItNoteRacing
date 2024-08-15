538e6bb0767c51cc84bba2772210efc322dbc546
Derrick Moeller <derrickmoeller@gmail.com>
2024-08-14 19:51:41 -0500

    Added Automobilista 2 to the list of unsupported games.

--- PostItNoteRacing.Plugin/Properties/AssemblyInfo.cs
+++ PostItNoteRacing.Plugin/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.5.10")]
-[assembly: AssemblyFileVersion("1.0.5.10")]
+[assembly: AssemblyVersion("1.0.5.11")]
+[assembly: AssemblyFileVersion("1.0.5.11")]
